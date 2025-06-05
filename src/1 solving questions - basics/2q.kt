fd53e27906a0e6ef9293c77429b0c5554d9fddaf
Ranjan-82P <priyanshu4282ranjan82@gmail.com>
2025-06-05 23:43:27 +0530

    practising and understanding better day 3

--- src/1 solving questions - basics/2q.kt
+++ src/1 solving questions - basics/2q.kt
@@ -1,1 +1,18 @@
 // Take two numbers as input and print their sum, difference, product, and division.
+
+fun main() {
+    val a = readLine()?.toIntOrNull()
+    val b = readLine()?.toIntOrNull()
+    if (a != null && b != null) {
+        val sum = a + b
+        val subtraction = a - b
+        val product = a * b
+        val division = a.toDouble() / b.toDouble()
+        print("Sum: $sum\n")
+        print("Subtraction: $subtraction\n")
+        print("Product: $product\n")
+        print("Division: $division\n")
+    } else {
+        println("Invalid input. Please enter valid integers.")
+    }
+}
